50c4909cbd0950b862b006c6ca9a8f1e3f100a20
gokhan <gokhancakicioglu@mantis.com.tr>
2022-03-27 21:29:02 +0300

    Posts are finished...

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auths");
+const postRoute = require("./routes/posts");
 
 const app = express();
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+app.use("/api/post", postRoute);
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
